a0c5716587923e0e742b15d7c4adc462e8f4fc57
Thiru <rthirucs@gmail.com>
2017-10-29 09:43:53 +0530

    removing the auth

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import userRoutes from './user.route';
-import authRoutes from './auth.route';
 import tournamentRoutes from './tournament.route';
 
 const router = express.Router(); // eslint-disable-line new-cap
@@ -15,7 +14,5 @@ router.use('/users', userRoutes);
 
 router.use('/tournaments', tournamentRoutes);
 
-// mount auth routes at /auth
-router.use('/auth', authRoutes);
 
 export default router;
